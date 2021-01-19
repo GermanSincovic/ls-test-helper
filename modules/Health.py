5730833a689cecdf3b139d620787e8e5b5cbb47d
Ivan Didyk <41524878+GermanSincovic@users.noreply.github.com>
2021-01-19 11:48:45 +0200

    Health-page exception handling

--- modules/Health.py
+++ modules/Health.py
@@ -1,39 +1,69 @@
 import requests
 from lxml import html
 
+from modules import Log
+
 
 def get_health_pages():
+    devtest_url = "http://ls-tools-ls-g.dev-i.net:8091/health-status/"
+    preprod_url = "https://preprod-component-monitoring.livescore.com/health-status/"
+    prod_url = "https://component-monitoring.livescore.com/health-status/"
+    loadtest_url = "http://35.246.114.214:8091/health-status/"
+    loadtest_iron_url = "http://35.246.114.214:8092/health-status/"
+    sr_keys_url = "http://ls-tools-ls-g.dev-i.net:8060/"
+
     health_data = []
 
-    devtest = requests.get("http://ls-tools-ls-g.dev-i.net:8091/health-status/").json()
-    preprod = requests.get("https://preprod-component-monitoring.livescore.com/health-status/").json()
-    prod = requests.get("https://component-monitoring.livescore.com/health-status/").json()
-    loadtest = requests.get("http://35.246.114.214:8091/health-status/").json()
-    loadtest_iron = {"loadtest-iron": requests.get("http://35.246.114.214:8092/health-status/").json()['loadtest']}
+    try:
+        devtest = requests.get(devtest_url).json()
+        for env in devtest:
+            health_data.append({env: devtest[env]})
+    except Exception as e:
+        Log.warning("Can't get data from {}, {}".format(devtest_url, e))
 
-    tree = html.fromstring(requests.get("http://ls-tools-ls-g.dev-i.net:8060/").content)
-    headers = list()
-    lines = list()
-    sr_keys = {"sr-keys": []}
-    for header in tree.xpath("//th/text()"):
-        headers.append(header)
-    for line in tree.xpath("//td/*/text()"):
-        lines.append(line)
-    for i in range(0, len(lines)):
-        if len(sr_keys["sr-keys"]) < i // len(headers) + 1:
-            sr_keys["sr-keys"].append({})
-        sr_keys["sr-keys"][i // len(headers)].update({headers[i % len(headers)]: lines[i]})
+    try:
+        preprod = requests.get(preprod_url).json()
+        for env in preprod:
+            health_data.append({env: preprod[env]})
+    except Exception as e:
+        Log.warning("Can't get data from {}, {}".format(preprod_url, e))
 
-    for env in devtest:
-        health_data.append({env: devtest[env]})
-    for env in preprod:
-        health_data.append({env: preprod[env]})
-    for env in prod:
-        health_data.append({env: prod[env]})
-    for env in loadtest:
-        health_data.append({env: loadtest[env]})
-    for env in loadtest_iron:
-        health_data.append({env: loadtest_iron[env]})
-    health_data.append(sr_keys)
+    try:
+        prod = requests.get(prod_url).json()
+        for env in prod:
+            health_data.append({env: prod[env]})
+    except Exception as e:
+        Log.warning("Can't get data from {}, {}".format(prod_url, e))
+
+    try:
+        loadtest = requests.get(loadtest_url).json()
+        for env in loadtest:
+            health_data.append({env: loadtest[env]})
+    except Exception as e:
+        Log.warning("Can't get data from {}, {}".format(loadtest_url, e))
+
+    try:
+        loadtest_iron = {"loadtest-iron": requests.get(loadtest_iron_url).json()['loadtest']}
+        for env in loadtest_iron:
+            health_data.append({env: loadtest_iron[env]})
+    except Exception as e:
+        Log.warning("Can't get data from {}, {}".format(loadtest_iron_url, e))
+
+    try:
+        tree = html.fromstring(requests.get(sr_keys_url).content)
+        headers = list()
+        lines = list()
+        sr_keys = {"sr-keys": []}
+        for header in tree.xpath("//th/text()"):
+            headers.append(header)
+        for line in tree.xpath("//td/*/text()"):
+            lines.append(line)
+        for i in range(0, len(lines)):
+            if len(sr_keys["sr-keys"]) < i // len(headers) + 1:
+                sr_keys["sr-keys"].append({})
+            sr_keys["sr-keys"][i // len(headers)].update({headers[i % len(headers)]: lines[i]})
+        health_data.append(sr_keys)
+    except Exception as e:
+        Log.warning("Can't get data from {}, {}".format(sr_keys_url, e))
 
     return health_data, 200
